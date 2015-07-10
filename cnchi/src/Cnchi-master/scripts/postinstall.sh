6692e0f8be01ea0a49675837be6c95198302c9f1
agajews <agajews@gmail.com>
2015-07-10 13:29:18 -0500

    Updated Cnchi Postinstall

--- cnchi/src/Cnchi-master/scripts/postinstall.sh
+++ cnchi/src/Cnchi-master/scripts/postinstall.sh
@@ -140,81 +140,50 @@ postinstall()
         sed -i "s/'us'/'${KEYBOARD_LAYOUT}'/" /usr/share/cnchi/scripts/set-settings
     fi
     # Set gsettings
-    rm /usr/share/applications/bssh.desktop
-	rm /usr/share/applications/bvnc.desktop
-	rm /usr/share/applications/avahi-discover.desktop
-	rm /usr/share/applications/qv4l2.desktop
-	rm /usr/share/applications/polkit-gnome-authentication-agent-1.desktop
-	rm /usr/share/applications/tracker-needle.desktop
-	rm /usr/share/applications/zenmap.desktop
-	rm /usr/share/applications/zenmap-root.desktop
-	rm /usr/share/applications/gksu.desktop
-	rm /usr/share/applications/gucharmap.desktop
-	rm /usr/share/applications/cups.desktop
-	rm /usr/share/applications/uxterm.desktop
-	rm /usr/share/applications/epiphany.desktop
-	rm /usr/share/applications/empathy.desktop
-	rm /usr/share/applications/designer-qt4.desktop
-	rm /usr/share/applications/linguist-qt4.desktop
-	rm /usr/share/applications/assistant-qt4.desktop
-	rm /usr/share/applications/qdbusviewer-qt4.desktop
-	sed -i 's@/usr/share/argon/argon.png@gnome-app-install@' /usr/share/applications/argon.desktop
-	sed -i 's@/usr/share/argon/argon.png@gnome-app-install@' /usr/share/applications/argon-notifier-config.desktop
-	sed -i 's@Icon=gnome-books@Icon=unity-webapps-amazoncloudreader@' /usr/share/applications/org.gnome.Books.desktop
-	sed -i 's@Icon=builder@Icon=textwrangler@' /usr/share/applications/org.gnome.Builder.desktop
-	sed -i 's@Icon=gnome-characters@Icon=accessories-character-map@' /usr/share/applications/org.gnome.Characters.desktop
-	sed -i 's@Icon=x-office-address-book@Icon=evolution-addressbook@' /usr/share/applications/org.gnome.Contacts.desktop
-	sed -i 's@Icon=grsync.png@Icon=luckybackup@' /usr/share/applications/grsync.desktop
-	sed -i 's@Icon=xterm-color_48x48@Icon=xorg@' /usr/share/applications/xterm.desktop
-	sed -i 's@Icon=tracker@Icon=preferences-system-search@' /usr/share/applications/tracker-preferences.desktop
-	cp -f /etc/apricity-assets/playonlinux.png /usr/share/playonlinux/etc
-	cp -f /etc/apricity-assets/playonlinux15.png /usr/share/playonlinux/etc
-	cp -f /etc/apricity-assets/playonlinux16.png /usr/share/playonlinux/etc
-	cp -f /etc/apricity-assets/playonlinux22.png /usr/share/playonlinux/etc
-	cp -f /etc/apricity-assets/playonlinux32.png /usr/share/playonlinux/etc
-	set -e -u
-	sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
-	locale-gen
-	ln -sf /usr/share/zoneinfo/UTC /etc/localtime
-	usermod -s /usr/bin/zsh root
-	cp -aT /etc/skel/ /root/
-	chmod 700 /root
-	echo 'created user'
-	chmod 750 /etc/sudoers.d
-	chmod 440 /etc/sudoers.d/g_wheel
-	chown -R root /etc/sudoers.d
-	echo 'sudoed'
-	EDITOR=nano
-	#/etc/apricity-assets/Elegant_Dark/install.sh
-	# Set Apricity name in filesystem files
-	sed -i.bak 's/Arch Linux/Apricity OS/g' /usr/lib/os-release
-	sed -i.bak 's/arch/apricity/g' /usr/lib/os-release
-	sed -i.bak 's/www.archlinux.org/www.apricityos.com/g' /usr/lib/os-release
-	sed -i.bak 's/bbs.archlinux.org/www.apricityos.com/g' /usr/lib/os-release
-	sed -i.bak 's/bugs.archlinux.org/www.apricityos.com/g' /usr/lib/os-release
-	cp -f /usr/lib/os-release /etc/os-release
-	arch=`uname -m`
-	if [ "$arch" == "x86_64" ]
-	then
-	    echo "x86_64, plymouth okay"
-	    echo "$(cat /etc/mkinitcpio.conf)"
-	    sed -i.bak 's/base udev/base udev plymouth/g' /etc/mkinitcpio.conf
-	    chown -R root.root /usr/share/plymouth/themes/apricity
-	    plymouth-set-default-theme -R apricity
-	    mkinitcpio -p linux
-	    systemctl enable org.cups.cupsd.service
-	    systemctl enable smbd nmbd
-	else
-	    echo "i686, no plymouth"
-	fi
-	sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
-	sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
-	echo 'seded'
-	systemctl enable graphical.target gdm.service pacman-init.service dhcpcd.service
-	systemctl -fq enable NetworkManager ModemManager
-	systemctl set-default graphical.target
-	systemctl enable tlp.service tlp-sleep.service
-	systemctl mask systemd-rfkill@.service
+    rm ${DESTDIR}/usr/share/applications/bssh.desktop
+	rm ${DESTDIR}/usr/share/applications/bvnc.desktop
+	rm ${DESTDIR}/usr/share/applications/avahi-discover.desktop
+	rm ${DESTDIR}/usr/share/applications/qv4l2.desktop
+	rm ${DESTDIR}/usr/share/applications/polkit-gnome-authentication-agent-1.desktop
+	rm ${DESTDIR}/usr/share/applications/tracker-needle.desktop
+	rm ${DESTDIR}/usr/share/applications/zenmap.desktop
+	rm ${DESTDIR}/usr/share/applications/zenmap-root.desktop
+	rm ${DESTDIR}/usr/share/applications/gksu.desktop
+	rm ${DESTDIR}/usr/share/applications/gucharmap.desktop
+	rm ${DESTDIR}/usr/share/applications/cups.desktop
+	rm ${DESTDIR}/usr/share/applications/uxterm.desktop
+	rm ${DESTDIR}/usr/share/applications/epiphany.desktop
+	rm ${DESTDIR}/usr/share/applications/empathy.desktop
+	rm ${DESTDIR}/usr/share/applications/designer-qt4.desktop
+	rm ${DESTDIR}/usr/share/applications/linguist-qt4.desktop
+	rm ${DESTDIR}/usr/share/applications/assistant-qt4.desktop
+	rm ${DESTDIR}/usr/share/applications/qdbusviewer-qt4.desktop
+	sed -i 's@/usr/share/argon/argon.png@gnome-app-install@' ${DESTDIR}/usr/share/applications/argon.desktop
+	sed -i 's@/usr/share/argon/argon.png@gnome-app-install@' ${DESTDIR}/usr/share/applications/argon-notifier-config.desktop
+	sed -i 's@Icon=gnome-books@Icon=unity-webapps-amazoncloudreader@' ${DESTDIR}/usr/share/applications/org.gnome.Books.desktop
+	sed -i 's@Icon=builder@Icon=textwrangler@' ${DESTDIR}/usr/share/applications/org.gnome.Builder.desktop
+	sed -i 's@Icon=gnome-characters@Icon=accessories-character-map@' ${DESTDIR}/usr/share/applications/org.gnome.Characters.desktop
+	sed -i 's@Icon=x-office-address-book@Icon=evolution-addressbook@' ${DESTDIR}/usr/share/applications/org.gnome.Contacts.desktop
+	sed -i 's@Icon=grsync.png@Icon=luckybackup@' ${DESTDIR}/usr/share/applications/grsync.desktop
+	sed -i 's@Icon=xterm-color_48x48@Icon=xorg@' ${DESTDIR}/usr/share/applications/xterm.desktop
+	sed -i 's@Icon=tracker@Icon=preferences-system-search@' ${DESTDIR}/usr/share/applications/tracker-preferences.desktop
+	sed -i 's@Icon=sbackup-restore@Icon=grsync-restore@' ${DESTDIR}/usr/share/applications/sbackup-restore.desktop
+	sed -i 's@Icon=sbackup-restore@Icon=grsync@' ${DESTDIR}/usr/share/applications/sbackup-config.desktop
+	sed -i 's@Icon=sbackup-restore@Icon=grsync-restore@' ${DESTDIR}/usr/share/applications/sbackup-restore-su.desktop
+	sed -i 's@Icon=sbackup-restore@Icon=grsync@' ${DESTDIR}/usr/share/applications/sbackup-config-su.desktop
+	cp -f ${DESTDIR}/etc/apricity-assets/playonlinux.png ${DESTDIR}/usr/share/playonlinux/etc
+	cp -f ${DESTDIR}/etc/apricity-assets/playonlinux15.png ${DESTDIR}/usr/share/playonlinux/etc
+	cp -f ${DESTDIR}/etc/apricity-assets/playonlinux16.png ${DESTDIR}/usr/share/playonlinux/etc
+	cp -f ${DESTDIR}/etc/apricity-assets/playonlinux22.png ${DESTDIR}/usr/share/playonlinux/etc
+	cp -f ${DESTDIR}/etc/apricity-assets/playonlinux32.png ${DESTDIR}/usr/share/playonlinux/etc
+	sed -i.bak 's/Arch Linux/Apricity OS/g' ${DESTDIR}/usr/lib/os-release
+	sed -i.bak 's/arch/apricity/g' ${DESTDIR}/usr/lib/os-release
+	sed -i.bak 's/www.archlinux.org/www.apricityos.com/g' ${DESTDIR}/usr/lib/os-release
+	sed -i.bak 's/bbs.archlinux.org/www.apricityos.com/g' ${DESTDIR}/usr/lib/os-release
+	sed -i.bak 's/bugs.archlinux.org/www.apricityos.com/g' ${DESTDIR}/usr/lib/os-release
+	cp -f ${DESTDIR}/usr/lib/os-release ${DESTDIR}/etc/os-release
+	sed -i.bak 's/base udev/base udev plymouth/g' ${DESTDIR}/etc/mkinitcpio.conf
+	chown -R root.root ${DESTDIR}/usr/share/plymouth/themes/apricity
 	cp -R ${DESTDIR}/home/${USER_NAME}/.config ${DESTDIR}/etc/skel
 	chroot ${DESTDIR} su -c xdg-user-dirs-update ${USER_NAME}
 
